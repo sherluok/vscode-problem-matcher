d0f24307818b4fb9f955ea52342d4215c8fbd545
sherluok <sherluok@126.com>
2025-11-11 16:37:41 +0800

    contribute brand icons

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 
 ## VS Code Extension
 
-https://marketplace.visualstudio.com/items?itemName=sherluok.problem-matcher
+A vscode extension published under id [`sherluok.problem-matcher`](https://marketplace.visualstudio.com/items?itemName=sherluok.problem-matcher) at marketplace.
+
+```sh
+code --install-extension sherluok.problem-matcher
+```
+
+[Doc](./src/vscode/README.md)
+
+## NPM Package
+
+A npm package published under id [`@sherluok/vscode-problem-matcher`](https://www.npmjs.com/package/@sherluok/vscode-problem-matcher) at npm registry.
+
+```sh
+pnpm add @sherluok/vscode-problem-matcher
+```
+
+[Doc](./src/npm/README.md)
+
+## Development
+
+### vscode
 
 ```sh
 pnpm run vscode:build
@@ -12,14 +32,15 @@ pnpm dlx vsce login sherluok
 pnpm dlx vsce publish
 ```
 
-### Bundle Extension
+#### Bundle Extension
 
 ```sh
-pnpm run build
-pnpm exec vsce package
+pnpm run vscode:build
+cd ./out/vscode
+pnpm dlx vsce package
 ```
 
-### Install Extension from Bundled
+#### Install Extension from Bundled
 
 ```sh
 code --install-extension ./problem-matcher-0.2.0.vsix
@@ -27,13 +48,13 @@ code --install-extension ./problem-matcher-0.2.0.vsix
 
 > or run vscode task: install
 
-### Install Extension from Marketplace
+#### Install Extension from Marketplace
 
 ```sh
 code --install-extension sherluok.problem-matcher
 ```
 
-### Publish Extension
+#### Publish Extension
 
 https://code.visualstudio.com/api/working-with-extensions/publishing-extension
 
@@ -45,10 +66,7 @@ pnpm exec vsce publish
 
 Open https://marketplace.visualstudio.com/manage
 
-
-## NPM Package
-
-https://www.npmjs.com/package/@sherluok/vscode-problem-matcher
+### npm
 
 ```sh
 pnpm run npm:build
--- src/vscode/build.ts
+++ src/vscode/build.ts
@@ -1,12 +1,17 @@
 import { context as esbuildContext, type BuildOptions, type Plugin } from 'esbuild';
+import { cpSync, mkdirSync, readdirSync, writeFileSync } from 'node:fs';
 import { cp, rm, writeFile } from 'node:fs/promises';
-import { join, resolve } from 'node:path';
+import { basename, join, posix, resolve, win32 } from 'node:path';
+import { webfont } from 'webfont';
 import { default as packageJSON } from '../../package.json' with { type: 'json' };
 import { vscodeTaskPlugin } from '../npm/esbuild';
 import { default as manifestJSON } from './manifest.json' with { type: 'json' };
 
-const srcDir = resolve('src/vscode');
-const outDir = resolve('out/vscode');
+const workspaceRoot = process.cwd();
+const packageRoot = __dirname;
+
+const srcDir = join(workspaceRoot, 'src/vscode');
+const outDir = join(workspaceRoot, 'out/vscode');
 
 const buildOptions: BuildOptions = {
   entryPoints: [
@@ -27,17 +32,58 @@ const buildOptions: BuildOptions = {
   ],
 };
 
-async function cleanOutputs() {
+async function cleanOutputs(): Promise<void> {
   await rm(outDir, {
     recursive: true,
     force: true,
   });
 }
 
-async function copyTemplates() {
+async function contributeIcons() {
+  const searchDir = join(workspaceRoot, 'src/vscode/icons/brand');
+  const svgFiles = readdirSync(searchDir).filter((it) => it.endsWith('.svg')).map((it) => join(searchDir, it).replaceAll(win32.sep, posix.sep));
+  const fontResult = await webfont({
+    files: svgFiles,
+    formats: ['woff'],
+    startUnicode: 0xE000,
+    fontHeight: 1600,
+    verbose: true,
+    normalize: true,
+    sort: false
+  });
+
+  mkdirSync(join(workspaceRoot, 'out/vscode/icons/'), { recursive: true });
+  cpSync(join(workspaceRoot, 'src/vscode/icons/brand.html'), join(workspaceRoot, 'out/vscode/icons/brand.html'));
+  writeFileSync(join(workspaceRoot, 'out/vscode/icons/brand.woff'), fontResult.woff!, 'binary');
+
+  mkdirSync(join(workspaceRoot, 'examples/icons/.vscode'), { recursive: true });
+  writeFileSync(join(workspaceRoot, 'examples/icons/.vscode/tasks.json'), JSON.stringify({
+    version: '2.0.0',
+    tasks: svgFiles.map((it) => ({
+      label: `brand-${basename(it).replace(/\.svg$/, '')}`,
+      type: 'shell',
+      command: 'pwd',
+      icon: {
+        id: `brand-${basename(it).replace(/\.svg$/, '')}`,
+      },
+    })),
+  }, null, 2));
+
+  return Object.fromEntries(svgFiles.map((it, i) => [`brand-${basename(it).replace(/\.svg$/, '')}`, {
+    description: `Sherluok's brand icon`,
+    default: {
+      fontPath: './icons/brand.woff',
+      fontCharacter: `\\e${i.toString(16).padStart(3, '0')}`,
+    },
+  }]));
+}
+
+async function copyTemplates(): Promise<void> {
   await cp(resolve('LICENSE'), join(outDir, 'LICENSE'));
   await cp(join(srcDir, 'README.md'), join(outDir, 'README.md'));
   await cp(join(srcDir, 'icon.png'), join(outDir, 'icon.png'));
+
+  const contributesIcons = await contributeIcons();
 
   const extensionPackageJSON = Object.assign({
     version: packageJSON.version,
@@ -47,7 +93,12 @@ async function copyTemplates() {
     engines: {
       vscode: packageJSON.devDependencies['@types/vscode'],
     },
-  }, manifestJSON);
+  }, manifestJSON, {
+    contributes: {
+      ...manifestJSON.contributes,
+      icons: contributesIcons,
+    },
+  });
 
   await writeFile(join(outDir, 'package.json'), JSON.stringify(extensionPackageJSON, null, 2));
 }
@@ -101,7 +152,15 @@ async function build() {
 if (require.main === module) {
   if (process.argv.includes('--watch')) {
     watch();
+  } else if (process.argv.includes('--fonts')) {
+    contributeIcons().then(console.log).catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
   } else {
-    build();
+    build().catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
   }
 }
